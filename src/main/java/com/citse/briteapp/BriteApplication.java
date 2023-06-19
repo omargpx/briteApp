7dbbad8541d12e2cd0ac8d762a424657c962dd95
omargpx <omarguerrero@upeu.edu.pe>
2023-06-19 14:59:36 -0500

    structure

---
+++ src/main/java/com/citse/briteapp/BriteApplication.java
@@ -0,0 +1,13 @@
+package com.citse.briteapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BriteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BriteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/citse/briteapp/BriteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.citse.briteapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BriteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
